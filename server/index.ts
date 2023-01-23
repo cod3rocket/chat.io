e58436b0e8e0291da72d4c01cbb3358db58d493f
Eduardo Teles <edu.teles@pm.me>
2023-01-23 19:13:39 -0300

    fix(server): add cors definition

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,12 @@
 import { serve } from 'https://deno.land/std@0.166.0/http/server.ts';
 import { Server } from 'https://deno.land/x/socket_io@0.2.0/mod.ts';
 
-const io = new Server();
+const io = new Server({
+	cors: {
+		origin: Deno.env.get('WEBAPP_URL') ?? 'http://localhost:5173',
+		methods: ['GET', 'POST']
+	}
+});
 
 io.on('connection', (socket) => {
 	console.log(`socket ${socket.id} connected`);
