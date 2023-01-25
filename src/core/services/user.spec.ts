886ef9eb8d74c784cf17b78804f6b2e5588f02b2
Henrique GC <henriq4net@gmail.com>
2023-01-25 09:00:18 -0300

    test: user should hava e hash identifier in name

--- src/core/services/user.spec.ts
+++ src/core/services/user.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-import { setUsername } from './user';
+import { setUsername, usernameWritable } from './user';
 
 describe('username', () => {
 	it('should user have a name', () => {
@@ -10,5 +10,18 @@ describe('username', () => {
 		setUsername(username);
 
 		expect(username).toBe('username');
+	});
+
+	it('should user have a hash identifier in name', () => {
+		let username = 'username';
+		const usernameRegex = new RegExp(`${username}#([0-9]{4})`);
+
+		setUsername(username);
+
+		usernameWritable.subscribe((value) => {
+			username = value;
+		});
+
+		expect(username).toMatch(usernameRegex);
 	});
 });
