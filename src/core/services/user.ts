fd1d264f8edd9c478a3604de36ccf7afc022abf3
Henrique GC <henriq4net@gmail.com>
2023-01-24 23:30:40 -0300

    fix: add a four algorism hash to identify a user when equals names are in confict

--- src/core/services/user.ts
+++ src/core/services/user.ts
@@ -3,5 +3,7 @@ import { writable } from 'svelte/store';
 export const usernameWritable = writable('');
 
 export function setUsername(newUsername: string) {
-	usernameWritable.set(newUsername);
+	const hash = Math.floor(Math.random() * 1000) + 4000;
+
+	usernameWritable.set(`${newUsername}#${hash}`);
 }
